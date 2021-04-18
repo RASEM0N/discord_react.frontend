459a75f413d908bff9dd71ca2b1038fc545bd65b
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-04-18 13:26:48 +0500

    "Начальная авторизация"

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Routes from './components/Routes'
+import { fbAuth } from './firebase/config'
+import { useHistory } from 'react-router-dom'
 
 const App = () => {
+    const history = useHistory()
+    useEffect(() => {
+        fbAuth.onAuthStateChanged((user) => {
+            if (user) {
+                history.push('/')
+            } else {
+                history.push('/login')
+            }
+        })
+    }, [])
+
     return (
         <div>
             <Routes />
--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Main() {
-    return <div className="App">Main</div>
+    return (
+        <div
+            className="App"
+            style={{
+                marginTop: 100,
+                alignItems: 'center',
+                display: 'flex',
+                flexDirection: 'column',
+            }}
+        >
+            Main
+            <Link to={'/login'}>Login</Link>
+            <Link to={'/password'}>Password</Link>
+        </div>
+    )
 }
 
 export default Main
